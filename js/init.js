29b739dfdf97187d2a9b589c1b9aa18a4c9b15c6
Shaunett Daley <sdaley@cdcservices.net>
2020-02-10 16:53:02 -0500

    Adding questions

--- js/init.js
+++ js/init.js
@@ -5,3 +5,35 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+document.addEventListener('DOMContentLoaded',()=> {
+  console.log('testying');
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+})
+
+
+//checking answers
+let correctAnswers = ['C','B','D'];
+let form = document.querySelector('.quiz-form');
+
+//listen when submit button click
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  let score = 0;
+  const userAnswers = [form.q1.value,form.q2.value,form.q3.value];
+  let satus = document.querySelector('.status.mark');
+  userAnswers.forEach((answer, index)=> {
+    if(answer === correctAnswers[index]){
+      score +=10;
+      //show the symbol
+     // status.textContent = `<i class="fas fa-check"></i>`;
+    }
+    //status.textContent = `<i class="fas fa-check"></i>`;
+   
+  });
+    
+
+  console.log(score);
+})
