e122845b5a548ec7adadef3c71bc6237986c9aff
Shaunett Daley <sdaley@cendyn.com>
2020-02-25 23:45:36 -0500

    update  js to show popup

--- js/init.js
+++ js/init.js
@@ -7,8 +7,52 @@
 })(jQuery); // end of jQuery name space
 
 
+//detect if user click the close  button
+let popupForm = document.querySelector('.popup-form');
+let exitButton = document.querySelector('.exit-test');
+let exitTestPopup = document.querySelector('.popup-wrapper-exit');
+let mainPopup = document.querySelector('.popup-wrapper');
+let takeTest = document.querySelector('.take-test');
+
+exitButton.addEventListener('click', e => {
+  e.preventDefault();
+  console.log(' exit button click');
+  exitTestPopup.style.display ="block";
+  mainPopup.style.display = "none";
+  closingWindow();
+})
+
+
+takeTest.addEventListener('click', e => {
+  e.preventDefault();
+  console.log(' disregard button click');
+  exitTestPopup.style.display ="none";
+  mainPopup.style.display = "block";
+})
+
+
+let closingWindow = ()=> {
+  setTimeout(()=> {
+    window.close();
+  },5000);
+}
+
+
+//get value from form
+let showname = document.querySelector('.info');
+popupForm.addEventListener('submit', e => {
+  e.preventDefault();
+  //get the value
+  let name = popupForm.firstname.value.trim();
+  console.log(name);
+  mainPopup.style.display = "none";
+showname.textContent = name;
+})
+
+
+
+
 document.addEventListener('DOMContentLoaded',()=> {
-  console.log('testying');
   let modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
 })
@@ -29,6 +73,7 @@ form.addEventListener('submit', e => {
   userAnswers.forEach((answer, index)=> {
     console.log(index);
     if(answer === correctAnswers[index]){
+     // answer[correctAnswers[index]].style.color = "red";
             score +=10;
         
     }
